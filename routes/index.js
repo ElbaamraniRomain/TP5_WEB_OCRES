b722a7b811b0538bc96bd4302eea65dc29fe40a8
romain <romainelbaamrani@gmail.com>
2020-11-12 14:18:47 +0100

    debut CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET */
-router.get('/movies', function(req, res, next){
-  res.render('index', { title: 'ifrjrfjreu' });
-});
-
-/* GET */
-router.get('/movies/:id', function(req, res, next) {
-  res.render('index', { title: req.body });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 
-/* PUT */
-router.put('/movies/', function(req, res, next) {
-  res.render('index', { title: req.params.id });
-});
-
-/* POST */
-router.put('/movies/', function(req, res, next) {
-  res.render('index', { title: req.params.id });
-});
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -6,5 +6,32 @@ let movies = [
         id: 'tt1375666',
         movie: 'Inception',
     }
-]
+];
+
+/* GET movie listing. */
+router.get('/', (req, res) => {
+    // Get List of user and return JSON
+    res.status(200).json({ movies });
+  });
+  
+/* GET */
+router.get('/movies', function(req, res, next){
+    res.render('index', { title: 'ifrjrfjreu' });
+  });
+  
+  /* GET */
+  router.get('/movies/:id', function(req, res, next) {
+    res.render('index', { title: req.body });
+  });
+  
+  
+  /* PUT */
+  router.put('/movies/', function(req, res, next) {
+    res.render('index', { title: req.params.id });
+  });
+  
+  /* POST */
+  router.put('/movies/', function(req, res, next) {
+    res.render('index', { title: req.params.id });
+  });
 module.exports = router;
