16a452a7be2878d57ec5f9947c5eae04f3ed5607
romain <romainelbaamrani@gmail.com>
2020-11-15 17:49:11 +0100

    add .gitignore

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var _ = require('lodash');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
 var moviesRouter = require('./routes/movies');
 
 
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// app.use('/', indexRouter);
 app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
